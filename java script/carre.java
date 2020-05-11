e6268879f28beacfd5b57cc337569cd442aaa612
unknown <adamakone>
2020-05-11 05:58:14 +0000

    commit 2

---
+++ java script/carre.java
@@ -0,0 +1,17 @@
+public class carre extends figure{
+
+	public double demiPerimetre(){
+		return this.longueur*2;
+	}
+	public double surface(){
+	   return this.coté*coté;	
+	}
+	public double diagonale(){
+	   return 0;
+	}
+
+
+
+
+
+}
--- java script/rectangle.java
+++ java script/rectangle.java
@@ -1,13 +1,17 @@
-import java.util.scanner;
-class rectangl
-{
-  public static void main(String []args)
-{
-  scanner kb=new scanner(System.in);
-  system.out.println("entrer la longueur et la largeur du rectangle");
-  int 1=kb.nextInt();
-  int b=kb.nextInt();
-  int ar=1*b;
-  system.out.println("area of rectangle is"+ar);
-}
+public class rectangle extends figure{
+
+	public double Perimetre(){
+		return this.longueur+largeur*2;
+	}
+	public double surface(){
+	   return this.longueur*largeur;	
+	}
+	public double diagonale(){
+	   return 0;
+	}
+
+
+
+
+
 }
